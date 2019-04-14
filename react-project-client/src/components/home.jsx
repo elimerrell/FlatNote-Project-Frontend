ba4cf896f998d0a05be694018ad1e06e6c7e1d52
omfgtora <spaminol@gmail.com>
2019-04-14 12:38:25 -0700

    save token to local storage and add to state

--- react-project-client/src/components/home.jsx
+++ react-project-client/src/components/home.jsx
@@ -5,34 +5,78 @@ import logo from "../logo.png";
 class Home extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      authToken: ''
+    };
   }
+
+  handleSubmit = (ev) => {
+    ev.preventDefault()
+
+    const newUser = {}
+
+    newUser.name = ev.target.name.value
+    newUser.email = ev.target.email.value
+    newUser.password = ev.target.password.value
+    newUser.password_confirmation = ev.target.password_confirmation.value
+
+    fetch('http://localhost:3000/api/v1/users', 
+      { method: 'POST',
+        headers: { 'Content-Type': 'application/json' }, 
+        body: JSON.stringify(newUser)
+      }).then(response => response.json()).then(user => this.authenticateUser(user))
+  }
+
+  authenticateUser = (user) => {
+    // debugger
+    fetch('http://localhost:3000/authenticate', 
+    { method: 'POST',
+      headers: { 'Content-Type': 'application/json' }, 
+      body: JSON.stringify({ 'email': user.email, 'password': user.password_digest})
+    }).then(response => response.json()).then(json => this.assignToken(json.auth_token))
+  }
+
+  assignToken = (token) => {
+    window.localStorage.setItem("authToken", token)
+    this.setStateToken(token)
+  }
+
+  setStateToken = (token) => {
+    this.setState({authToken: token})
+  }
+
+  checkToken = () => {
+    if (window.localStorage.getItem("authToken")) {
+      console.log(window.localStorage.getItem("authToken"))
+      this.setState({authToken: window.localStorage.getItem("authToken")})
+    } else {
+      console.log('Token not found')
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          {/* <div>
-            Icons made by{" "}
-            <a
-              href="https://www.flaticon.com/authors/smashicons"
-              title="Smashicons"
-            >
-              Smashicons
-            </a>{" "}
-            from{" "}
-            <a href="https://www.flaticon.com/" title="Flaticon">
-              www.flaticon.com
-            </a>{" "}
-            is licensed by{" "}
-            <a
-              href="http://creativecommons.org/licenses/by/3.0/"
-              title="Creative Commons BY 3.0"
-              target="_blank"
-            >
-              CC 3.0 BY
-            </a>
-          </div> */}
+            <form onSubmit={this.handleSubmit}>
+              <div>
+                <label>Name: </label>
+                <input type="text" name="name" />
+              </div>
+              <div>
+                <label>Email: </label>
+                 <input type="text" name="email" />
+              </div>
+              <div>
+                <label>Password: </label>
+                <input type="password" name="password" />
+              </div>
+              <div>
+                <label>Confirm: </label>
+                <input type="password" name="password_confirmation" />
+              </div>
+              <button type="submit">Sign-up</button>
+            </form>
           <Link to="/dashboard">
             <button>Dashboard</button>
           </Link>
