8a9800849edfb2ddc7f960322962233462035b20
omfgtora <spaminol@gmail.com>
2019-04-15 12:45:33 -0700

    Added edit modal notes on click

--- react-project-client/src/components/notebook.jsx
+++ react-project-client/src/components/notebook.jsx
@@ -57,22 +57,44 @@ class Notebook extends Component {
     ));
   };
 
-  handleChange = () => {};
+  handleChange = (ev) => {
+    const attr = ev.target.name
+    const value = ev.target.value
+    const currentNoteValue = this.state.currentNote
+    currentNoteValue[attr] = value
+    this.setState({currentNote: currentNoteValue})
+  };
+
+  patchNote = () => {
+    const note = this.state.currentNote
+    fetch(`http://localhost:3000/api/v1/notebooks/${this.state.currentNote.notebook_id}/notes/${this.state.currentNote.id}`,{
+      method: 'PATCH',  
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(note)
+    })
+    this.setState({show: false, currentNote: {}})
+  }
+
+  handleSubmit = (ev) => {
+    ev.preventDefauly()
+    this.patchNote()
+  }
 
   openModal = () => {
     return (
       <Container>
         <div className="modal-box">
           <Box>
+            <form action="submit" onSubmit={this.handleSubmit}>
             <input
               name="title"
               value={this.state.currentNote.title}
               onChange={this.handleChange}
             />
-            <textarea name="content" rows="10" cols="70">
-              {this.state.currentNote.content}
+            <textarea name="content" rows="10" cols="70" onChange={this.handleChange} value={this.state.currentNote.content}>
             </textarea>
-            <Button color="info">Update</Button>
+            <Button color="info" onClick={this.patchNote}>Update</Button>
+            </form>
           </Box>
         </div>
       </Container>
