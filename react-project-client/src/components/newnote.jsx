eee2f64f6049900152fa0d0ffd5f2e755ab56e7e
elimerrell <elijah.merrell@gmail.com>
2019-04-16 11:15:12 -0700

    added conditional alert to new note

--- react-project-client/src/components/newnote.jsx
+++ react-project-client/src/components/newnote.jsx
@@ -17,8 +17,15 @@ class NewNote extends Component {
     this.setState({
       title: ev.target.value
     });
-    let newReview = {};
-    newReview.title = this.state.title;
+    let newNote = {};
+    newNote.title = this.state.title;
+  };
+
+  clearInputs = () => {
+    this.setState({
+      title: "",
+      content: ""
+    });
   };
 
   showNoteForm = () => {
@@ -51,7 +58,9 @@ class NewNote extends Component {
 
             <div class="field is-grouped">
               <div class="control">
-                <button class="button is-link">Submit</button>
+                <button onClick={() => this.clearInputs} class="button is-link">
+                  Submit
+                </button>
               </div>
             </div>
           </form>
